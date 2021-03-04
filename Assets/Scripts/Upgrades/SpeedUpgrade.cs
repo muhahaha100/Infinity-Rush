fbf30c68a1cd227292c3e6648eaeba7b4e6bdab1
cwllight8 <cwllight8@gmail.com>
2021-03-03 18:04:59 -0700

    upgrade code done

---
+++ Assets/Scripts/Upgrades/SpeedUpgrade.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedUpgrade : Upgrade
+{
+
+    [SerializeField] float speedModifier = 2;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<Player>().movementSpeed *= speedModifier;
+            StartCoroutine(shutOffUpgrade(collision.gameObject.GetComponent<Player>()));
+        }
+    }
+
+    private IEnumerator shutOffUpgrade(Player p)
+    {
+        yield return new WaitForSeconds(activeTime);
+        p.movementSpeed /= speedModifier;
+    }
+}
---
+++ Assets/Scripts/Upgrades/Upgrade.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Upgrade : MonoBehaviour
+{
+    [SerializeField] protected float activeTime = 0;
+}
